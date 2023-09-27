2793352dfb93936be1e835ea47e06bed754df9e2
renatakrisna <krisna233@hotmail.com>
2023-09-26 23:22:50 -0300

    inicio de criação de tela

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from 'next/image'
+import Imagem1 from '../assets/Screenshot_1.png'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Davi
-    </main>
+    <div className="w-full h-screen flex items-stard">
+      <div className='relative w-1/2 h-full flex flex-col'>
+      <img src={Imagem1.src} className="w-full h-full object-contain"/>
+      </div>
+      <div className='w-full h-full bg-[#E0E0E0] flex flex-col p-10'>
+          <h1 className='text-base tet-[#060606]'>Brand</h1>
+      </div>
+    </div>
   )
 }
